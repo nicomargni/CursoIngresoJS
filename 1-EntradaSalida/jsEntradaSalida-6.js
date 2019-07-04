5a0f7d573095d443f6d3754e7303d0113f66951c
nicomargni <47321325+nicomargni@users.noreply.github.com>
2019-07-04 15:38:44 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+      var numero1;
+      var numero2;
+      var resultado;
+
+      numero1 = document.getElementById ("numeroUno").value;
+      numero2 = document.getElementById ("numeroDos").value;
+
+      resultado = parseInt (numero1) + parseInt (numero2);
+      alert ("La suma es " + resultado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,57 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var numero1;
+    var numero2;
+    var resultado;
+
+    
+    numero1 = document.getElementById ("numeroUno").value;
+    numero2 = document.getElementById ("numeroDos").value;
+
+    resultado = parseInt (numero1) + parseInt (numero2);
+    alert ("La suma es igual a " + resultado);
 }
 
 function restar()
 {
-	
+	var numero1;
+    var numero2;
+    var resultado;
+
+    
+    numero1 = document.getElementById ("numeroUno").value;
+    numero2 = document.getElementById ("numeroDos").value;
+
+    resultado = parseInt (numero1) - parseInt (numero2);
+    alert ("La resta es igual a " + resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numero1;
+    var numero2;
+    var resultado;
+
+    
+    numero1 = document.getElementById ("numeroUno").value;
+    numero2 = document.getElementById ("numeroDos").value;
+
+    resultado = parseInt (numero1) * parseInt (numero2);
+    alert ("La multiplicacion es igual a " + resultado);
 }
 
 function dividir()
 {
-	
+    var numero1;
+    var numero2;
+    var resultado;
+
+    
+    numero1 = document.getElementById ("numeroUno").value;
+    numero2 = document.getElementById ("numeroDos").value;
+
+    resultado = parseInt (numero1) / parseInt (numero2);
+    alert ("La division es igual a " + resultado);	
 }
 
